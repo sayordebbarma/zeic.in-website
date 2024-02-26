6eecbbed242743fba90125a75732b209059a3afd
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2024-02-26 14:32:33 +0530

    setting up some img and tailwindcss

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import zeicLogo from '../../assets/images/zeic_logo.png'
 import React from 'react'
+import zeicLogo from '../../assets/images/logo/zeic_logo.png'
+import homebg from '../../assets/images/background/homebg.png'
 import './HomePage.css'
 
 function HomePage(){
   return (
   <>
   <div>
+  <img src={homebg} />
     <img src={zeicLogo} className="logo" alt="Vite logo" />
   </div>
   <h1>zeic.in website</h1>
