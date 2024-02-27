55e470279e3e4d95ca818995646fe5a99b38d876
tushienla <tushijamir3@gmail.com>
2024-02-28 01:05:36 +0530

    Changed contact header color

--- src/components/contactHeader/ContactHeader.jsx
+++ src/components/contactHeader/ContactHeader.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ContactHeader = () => {
   return (
-    <div className='bg-blue-950 opacity-90 flex justify-between px-4 py-2'>
+    <div className='bg-gray-800 flex justify-between px-4 py-2'>
       <a className='text-white underline ml-6' href="mailto: example@example.com">example@example.com</a>
-      <a className='bg-blue-600 hover:bg-blue-500 text-white p-1 rounded mr-6' href="tel:+1234567890">Call now</a>
+      <a className='bg-red-600 hover:bg-red-800 text-white p-1 rounded mr-6' href="tel:+1234567890">Call now</a>
     </div>
   )
 }
