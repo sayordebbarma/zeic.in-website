8bd1ae12a5b3c58c2e78650c8db0cf54075b2c0b
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2024-02-27 22:26:34 +0530

    setting up contact Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import HomePage from './pages/home/HomePage.jsx';
 import ProductsPage from './pages/products/ProductsPage.jsx';
 import ContactPage from './pages/contact/ContactPage.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
+import ContactHeader from './components/contactHeader/ContactHeader';
 
 function App(){
   return (
   <>
+  <ContactHeader />
   <Navbar />
   {/* <HomePage/>
   <ProductsPage/>
--- src/components/contactHeader/ContactHeader.jsx
+++ src/components/contactHeader/ContactHeader.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const ContactHeader = () => {
   return (
-    <div>ContactHeader</div>
+    <div className='bg-rose-500	flex justify-between px-4 py-2'>
+      <a className='text-white underline ml-6' href="mailto: example@example.com">example@example.com</a>
+      <a className='bg-blue-700 hover:bg-blue-600 text-white p-1 rounded mr-6' href="tel:+1234567890">Call now</a>
+    </div>
   )
 }
 
