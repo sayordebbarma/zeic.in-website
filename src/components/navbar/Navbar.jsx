7b859278521846c382734ddfe01f463ef5564f1e
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2024-02-28 00:19:11 +0530

    setting up width of homePage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import zeicLogo from '../../assets/images/logo/zeic_logo.png'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 mx-6 absolute z-10 text-white'>
+    <div className='flex justify-between items-center w-11/12 h-20 px-4 mx-6 absolute z-10 text-white'>
       <div className='flex items-center'>
-        <img className='h-24' src={zeicLogo} alt="zeic logo" />
-        <h1 className='text-3xl font-bold'>ZEIC</h1>
+        <img className='h-20' src={zeicLogo} alt="zeic logo" />
+        <h1 className='text-3xl font-bold ml-2'>ZEIC</h1>
       </div>
       <ul className="flex">
         <li>Home</li>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -8,7 +8,7 @@ function HomePage(){
     <img className='w-full h-full object-cover'src={homebg} alt="" />
     <div className='absolute w-full h-full top-0 left-0 bg-gray-900/50'></div>
     <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
-      <h1 className='font-extrabold	text-9xl'>ZIP ENGINEERING<br/>&<br/>INNOVATION CENTRE</h1>
+      <h1 className='font-extrabold	text-8xl'>ZIP ENGINEERING<br/>&<br/>INNOVATION CENTRE</h1>
       <p className='text-sm italic font-thin'>An “All In One” centre with Innovation, Sustainability<br/> and Development zipped under one roof</p>
     </div>
   </div>
