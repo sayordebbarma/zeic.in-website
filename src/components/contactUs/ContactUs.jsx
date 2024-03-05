84315e051a4cce0c4ebc451f82b557dfc2eb552b
tushienla <tushijamir3@gmail.com>
2024-03-05 23:34:39 +0530

    Contact form created

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -1,16 +1,77 @@
-import React from 'react'
+import React from 'react';
 
 const ContactUs = () => {
   return (
-    <div>
-        <h1 className='text-5xl font-bold text-center text-gray-800 mb-8'>Contact Us</h1>
-        <div className='flex flex-col'>
-            <div>
-                <img src="" alt="T" />
-            </div>
+    <div className="relative h-screen">
+      <h1 className="text-5xl font-bold text-center text-gray-800 mb-8">Contact Us</h1>
+      <div className="container px-20 py-16 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <div className="relative mb-4">
+          <label htmlFor="firstName" className="leading-7 text-sm text-gray-600">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              required
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+          <label htmlFor="lastName" className="leading-7 text-sm text-gray-600">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              required
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+          <label htmlFor="phoneNumber" className="leading-7 text-sm text-gray-600">
+              Phone Number
+            </label>
+            <input
+              type="tel"
+              id="phoneNumber"
+              name="phoneNumber"
+              required
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+          <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="message" className="leading-7 text-sm text-gray-600">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              required
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            ></textarea>
+          </div>
+          <button className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-800 transition duration-300">
+            Submit
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
