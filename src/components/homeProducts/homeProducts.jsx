3b186f44133aeb6a77907715f6759e5b33e15356
sayordebbarma <sayorofficial8543@gmail.com>
2024-05-17 22:55:07 +0530

    made some minor changes is homeProducts

--- src/components/homeProducts/homeProducts.jsx
+++ src/components/homeProducts/homeProducts.jsx
@@ -13,7 +13,7 @@ const homeProducts = () => {
         Products
       </h1>
       <div className='flex justify-evenly'>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10'>
+        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10'>
           {products.map((product) => (
             <Link to='products' key={product.id} onClick={scrollToTop}>
               <div className='max-w-64 rounded-lg shadow bg-gray-800 border-gray-700 overflow-hidden hover:bg-gray-900 transform transition duration-300 ease-in-out hover:scale-105 h-120'>
