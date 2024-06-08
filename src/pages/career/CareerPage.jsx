c98811bd6ba15afa304fe422dc8cbb7af6c246d2
sayordebbarma <sayorofficial8543@gmail.com>
2024-06-08 14:47:40 +0530

    made some changes

--- src/pages/career/CareerPage.jsx
+++ src/pages/career/CareerPage.jsx
@@ -5,21 +5,20 @@ import CareerForm from './CareerForm';
 import Footer from '../../components/footer/Footer';
 import ContactBG from '../../assets/images/background/careerBG.png';
 
-
 const CareerPage = () => {
   return (
     <>
       <ContactHeader />
       <Navbar navbarClassName='bg-gray-800' />
-      <div className='mt-32 md:grid grid-cols-2'>
-      <div
-      className='hidden md:block rounded-r-3xl my-6'
-      style={{
-        backgroundImage: `url(${ContactBG})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    ></div>
+      <div className='mt-36 md:mt-32 md:grid grid-cols-2'>
+        <div
+          className='hidden md:block rounded-r-3xl my-6'
+          style={{
+            backgroundImage: `url(${ContactBG})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        ></div>
         <CareerForm />
       </div>
       <Footer />
